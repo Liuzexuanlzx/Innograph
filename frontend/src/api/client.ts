8b1f39f403898f36c3e4b92ced67e4fb1fb2d573
Liuzexuanlzx <124090402@link.cuhk.edu.cn>
2026-05-07 01:12:05 +0800

    修复部署问题：增加Celery并发数，设置前端超时

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const client = axios.create({
   baseURL: '/api/v1',
   headers: { 'Content-Type': 'application/json' },
+  timeout: 120000,
 });
 
 export default client;
